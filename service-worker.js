2f4ea38103daa4c40b98f502e394da63beaacdb3
PetrVymitac <vymetal85@gmail.com>
2025-11-20 18:32:18 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'preklikavaci-cache-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'img/obrazek1.png',
+  'img/obrazek2.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
